abfc0d15501a9fdfe6f97ce7f8fc690e8f218179
DimkaPlay <40357533+DimkaPlay@users.noreply.github.com>
2018-06-18 16:10:30 +0400

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const request = require('request');
 const robot = new Discord.Client();
 var p = "^"
 
+robot.on('ready', () => {
+    robot.user.setActivity('loading..',{ type: "PLAYING" })
+    robot.user.setStatus('dnd')
+    setTimeout(status1, 16000)
+    console.log('ready launched bot...')
+});
+
 bot.on('message',(message)=> {
 if(message.content== "^help") { 
 message.channel.send("***^help-помощь по командам***");
